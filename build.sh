fa08b00e57b53177e7dc9a248032c83886b5619a
devinzhu <devinzhu@tencent.com>
2021-06-22 16:59:57 +0800

    优化构建、推送脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,32 @@
 #! /bin/bash
 
-version=2.0
+build() {
+  echo "build -----"
+  mvn clean package -DskipTests
 
-mvn clean package -DskipTests
+  docker build -t zeebe-simple-monitor:${version} .
+}
 
-docker build -t zeebe-simple-monitor:${version} .
+push() {
+  echo "push image -----"
+  docker tag zeebe-simple-monitor:${version}   ccr.ccs.tencentyun.com/sre_pcg/zeebe-simple-monitor:${version}
+  docker push ccr.ccs.tencentyun.com/sre_pcg/zeebe-simple-monitor:${version}
+}
+
+version=2.0.1
+
+build
+
+
+if [ $# -gt 0 ] && [ $1=='push' ]; then
+
+  push
+
+fi
+
+
+
+
+
+
+
--- push.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-version=2.0
-
-docker tag zeebe-simple-monitor:${version}   ccr.ccs.tencentyun.com/sre_pcg/zeebe-simple-monitor:${version}
-docker push ccr.ccs.tencentyun.com/sre_pcg/zeebe-simple-monitor:${version}
-
